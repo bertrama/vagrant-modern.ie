1bb20ea0866ae59746af89de27af87bd027e6c3a
Albert Bertram <bertrama@umich.edu>
2015-10-03 16:48:52 -0400

    Added TODO section

--- setup.bat
+++ setup.bat
@@ -8,3 +8,21 @@ set WINRM_EXEC=call %SYSTEMROOT%\System32\winrm
 %WINRM_EXEC% set winrm/config/client/auth @{Basic="true"}
 %WINRM_EXEC% set winrm/config/service @{AllowUnencrypted="true"}
 %WINRM_EXEC% set winrm/config/service/auth @{Basic="true"}
+
+REM TODO: Do conditionals on the ver command to replace the different version .bat scripts
+REM Idea: http://malektips.com/xp_dos_0025.html#.VhAbQJcSU8o
+REM
+REM ver | find "VERSION_STRING" > nul
+REM if %ERRORLEVEL% == 0 goto ver_x
+REM
+REM :ver_x
+REM ...
+REM goto register_vista | register_later
+REM
+REM :register_vista
+REM ...
+REM goto done
+REM :register_later
+REM ...
+REM goto done
+REM :done
